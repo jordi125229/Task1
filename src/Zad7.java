074af6c55da143215f00a9c29cd50bf05e5a2bf0
jordi125229 <piotr.jedraszczyk12@gmail.com>
2026-01-24 10:37:47 +0100

    Task7

---
+++ src/Zad7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Zad7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj swój wiek:");
+        int age = scanner.nextInt();
+
+        String result = (age  >= 18) ? "Pełnoletni" : "Niepełnoletni";
+
+        System.out.println(result);
+    }
+}
